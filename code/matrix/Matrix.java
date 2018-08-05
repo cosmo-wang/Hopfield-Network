26fc1f9dd8d97ed73399fdebc9becc3255162594
Cosmo Wang <yw259@cs.washington.edu>
2018-08-04 21:08:04 -0700

    matrix in progress

--- code/matrix/Matrix.java
+++ code/matrix/Matrix.java
@@ -1,5 +1,32 @@
 package matrix;
 
-public class Matrix {
+import java.util.Arrays;
 
+public class Matrix {
+	private double[][] matrix;
+	private int size;
+	
+	public static final int DEFAULT_CAPACITY = 38;
+	
+	public Matrix(double[][] matrix) {
+		if (matrix == null || matrix.length == 0 || matrix.length != matrix[0].length) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = 0; i < matrix.length; i++ ) {
+			this.matrix[i] = Arrays.copyOf(matrix[i], DEFAULT_CAPACITY);
+		}
+		this.size = matrix.length;
+	}
+	
+	public String toString() {
+		String result = "";
+		for (int i = 0; i < size; i++) {
+			String currentRow = "";
+			for (int j = 0; j < size; j++) {
+				currentRow += matrix[i][j] + " ";
+			}
+			result += currentRow.trim() + "\n";
+		}
+		return result;
+	}
 }
